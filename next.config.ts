f1072945425710147bfc60f2b080cf3c0743efd6
Rafael <lealdevworks@gmail.com>
2024-12-28 12:51:36 -0300

    Atualizando configuração de imagens e ajustando a conexão com o banco de dados

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', '123.45.67.89'], // IP público do backend
+    remotePatterns: [
+      {
+        protocol: 'http', // ou 'https' dependendo do seu servidor
+        hostname: 'localhost', // IP ou domínio do backend
+        port: '', // se aplicável, caso esteja usando uma porta diferente
+      },
+      {
+        protocol: 'http', // ou 'https'
+        hostname: '123.45.67.89', // IP público do backend
+        port: '', // se necessário
+      },
+    ],
   },
 };
 
--- src/app/api/hello/route.js
+++
@@ -1,7 +0,0 @@
-
-export async function GET(request) {
-    return new Response(JSON.stringify({ message: 'Hello from Next.js 13!' }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  
